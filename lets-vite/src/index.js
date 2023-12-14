8265c814b4f4d703d7d0f3a6488b19c7715e5f96
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-12-14 23:52:37 +0300

    update

---
+++ lets-vite/src/index.js
@@ -0,0 +1,18 @@
+class Employee extends React.Component {
+    addEmployee = () => {
+        alert('Clicked on addEmployee Method');
+    }    
+
+    render() {
+        return <div>
+            <h2>
+            Employee Component
+            </h2>
+            <button onClick={this.addEmployee}>Add Employee</button>
+        </div>
+    }
+}
+
+const element1 = <Employee></Employee>
+
+ReactDOM.render(element1, document.getElementById('root'));
